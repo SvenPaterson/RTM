8b9dbece6f9805ac86817b13c511603771bf2f40
SvenPaterson <stephenjgarden@icloud.com>
2024-04-12 10:40:09 -0400

    accounted for onedrive desktop,

--- upload_files/upload_program.py
+++ upload_files/upload_program.py
@@ -1,14 +1,18 @@
-# Specify the project directory and the environment name
 import shutil
 from rtm_funcs import *
 from pathlib import Path
+
+# For users who are more comfortable uploading directly from VS Code
+# Run this script to upload the motor configuration to RTM test stand
 
 # ENSURE RTM PROJECT RESIDES IN "C:/RTM/upload_files"
 # IF NOT MOVE IT OR CHANGE THE PATH FOR project_dir
 project_dir = Path("C:/RTM")
 
-
 desktop = Path(os.path.join(os.environ["USERPROFILE"], "Desktop"))
+onedrive_desktop = Path(os.path.join(os.environ["USERPROFILE"], "OneDrive", "Desktop"))
+if onedrive_desktop.exists():
+    desktop = onedrive_desktop
 config_xlsx = project_dir / "upload_files" / "RTM_config.xlsx"
 new_config_xlsx = desktop / "RTM_config.xlsx"
 if not new_config_xlsx.exists():
@@ -17,7 +21,7 @@ if not new_config_xlsx.exists():
     print("Please edit it to reflect the desired test profile and re-run the 'RTM Upload' program.")
 
 
-config_xlsx = Path.home() / "Desktop" / "RTM_config.xlsx"
+config_xlsx = desktop / "RTM_config.xlsx"
 # Load configurations and steps from Excel
 steps, config = load_config_from_excel(config_xlsx)
 
