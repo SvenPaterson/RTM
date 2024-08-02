5fece55cc0dfc59c9e9d660e2fa0f52b774c84dc
SvenPaterson <stephenjgarden@icloud.com>
2024-08-02 15:01:10 -0400

    updates

--- upload_files/upload_program.py
+++ upload_files/upload_program.py
@@ -31,6 +31,6 @@ steps, config = load_config_from_excel(config_xlsx)
 
 # Generate the motor_config.h file
 generate_motor_config(steps, config)
-exit()
+
 # Compile and upload the project
 compile_and_upload(project_dir)
