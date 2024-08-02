bbbc6e4ee7189b4b5b3a3e2f47d709078b271b8b
SvenPaterson <stephenjgarden@icloud.com>
2024-08-02 16:15:41 -0400

    quality of life for stephen

--- upload_files/upload_program.py
+++ upload_files/upload_program.py
@@ -7,19 +7,26 @@ from pathlib import Path
 
 # ENSURE RTM PROJECT RESIDES IN "C:/RTM/upload_files"
 # IF NOT MOVE IT OR CHANGE THE PATH FOR project_dir
-project_dir = Path("C:/RTM")
+
+user_profile = Path(os.environ["USERPROFILE"])
+if user_profile == Path("C:/Users/Stephen.Garden"): # purely quality of life fix for Stephen...
+    project_dir = user_profile / "RTM"
+else:
+    project_dir = Path("C:/RTM")
 
 desktop = Path(os.path.join(os.environ["USERPROFILE"], "Desktop"))
-onedrive_desktop = Path(os.path.join(os.environ["USERPROFILE"], "OneDrive", "Desktop"))
+onedriveTSS_desktop = Path(os.path.join(os.environ["USERPROFILE"], "OneDrive - Trelleborg AB", "Desktop"))
+onedrive_desktop = Path(os.path.join(os.environ["USERPROFILE"], "Desktop"))
 if onedrive_desktop.exists():
     desktop = onedrive_desktop
+if onedriveTSS_desktop.exists():
+    desktop = onedriveTSS_desktop
 config_xlsx = project_dir / "upload_files" / "RTM_config.xlsx"
 new_config_xlsx = desktop / "RTM_config.xlsx"
 if not new_config_xlsx.exists():
     shutil.copy(config_xlsx, desktop)
     print("'RTM_config.xlsx' was not present on the desktop, so a generic one has been created.")
     print("Please edit it to reflect the desired test profile and re-run the 'RTM Upload' program.")
-
 
 config_xlsx = desktop / "RTM_config.xlsx"
 # Load configurations and steps from Excel
